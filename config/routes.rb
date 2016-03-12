affb021617f3aa2be5277d98eb6c6b41eeaae556
striking <chris@strikinggroup.com.au>
2016-03-13 09:52:52 +1000

    added styling to task logic

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'pages/home'
   get 'about' => 'pages#about'
+  get 'test' => 'pages#test'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
